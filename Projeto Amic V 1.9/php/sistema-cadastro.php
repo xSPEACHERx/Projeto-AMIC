439305dbea04fd3152f2d37f14959a81c2d18ad9
Diego Oliveira <72594684+xSPEACHERx@users.noreply.github.com>
2022-12-06 12:41:18 -0300

    correção de bug
     a troca de versão do php fez que as funcionalidades de vericar a senha e o email paracem de funcionar, essa é a correção.

--- Projeto Amic V 1.9/php/sistema-cadastro.php
+++ Projeto Amic V 1.9/php/sistema-cadastro.php
@@ -35,54 +35,59 @@ $pdo = require_once('../db/conecta.php');
 
     // se existe uma conta com esse email então uma mensagem de erro aparece
 
-        if ($dados != NULL){
+        if (count($dados) > 0){
             header("Location: ../cadastro.php?error=1$linklog");
-        } elseif ($dados2 != NULL) {
+
+        } elseif (count($dados2) > 0) {
             header("Location: ../cadastro.php?error=1$linklog");
-        } 
 
-// verifica se a confirmação de senha está correto
+        } else {
 
-    if($form == '1'){
-        $senha1 = $_POST['cadastro-usuario-senha'];
-        $senha2 = $_POST['cadastro-usuario-confirmar-senha'];
-    } elseif ($form == '2') {
-        $senha1 = $_POST['cadastro-tecnico-senha'];
-        $senha2 = $_POST['cadastro-tecnico-confirma-senha'];
-    }
+        // verifica se a confirmação de senha está correto
+            if($form == '1'){
+                $senha1 = $_POST['cadastro-usuario-senha'];
+                $senha2 = $_POST['cadastro-usuario-confirmar-senha'];
+            } elseif ($form == '2') {
+                $senha1 = $_POST['cadastro-tecnico-senha'];
+                $senha2 = $_POST['cadastro-tecnico-confirma-senha'];
+            }
 
-    if ($senha1 != $senha2){
-        header("Location: ../cadastro.php?error=2$linklog");
-    }
+            if ($senha1 == $senha2){
 
-// registra a conta
+                // registra a conta
+                    if ($form == '1') {
 
-    if ($form == '1') {
+                        $matricula = $_POST['cadastro-usuario-matricula'];
+                        $curso = $_POST['cadastro-usuario-curso'];
+                        $periodo = $_POST['cadastro-usuario-periodo'];
+                        $nome = $_POST['cadastro-usuario-nome'];
+                        $senha = $senha1;
+                        $login = $_POST['cadastro-usuario-login'];
+                        $relat = 0;
+                        
+                        $result = $pdo->query("INSERT INTO usuario VALUES ('{$matricula}', '{$curso}', '{$periodo}', '{$nome}', '{$email}', '{$senha}', '{$login}', '{$relat}')");
 
-        $matricula = $_POST['cadastro-usuario-matricula'];
-        $curso = $_POST['cadastro-usuario-curso'];
-        $periodo = $_POST['cadastro-usuario-periodo'];
-        $nome = $_POST['cadastro-usuario-nome'];
-        $senha = $senha1;
-        $login = $_POST['cadastro-usuario-login'];
-        $relat = 0;
+                        header("Location: ../login.php?success=true$linklog");
+
+                    } elseif ($form == '2'){
+
+                        $nome = $_POST['cadastro-tecnico-nome'];
+                        $email = $_POST['cadastro-tecnico-email'];
+                        $login = $_POST['cadastro-tecnico-login'];
+                        $siape = $_POST['cadastro-tecnico-siape'];
+                        $senha = $senha1;
+                        $isTec = 0; // istec é o numero de reportes que o tecnico respondeu
+
+                        $result = $pdo->query("INSERT INTO tecnico VALUES ('{$siape}', '{$nome}', '{$email}', '{$senha}', '{$login}', '{$isTec}')");
+
+                        header("Location: ../login.php?success=true$linklog");
+                    }
+
+
+                } else{
+                    header("Location: ../cadastro.php?error=2$linklog");
+                    }
+            }
+
         
-        $result = $pdo->query("INSERT INTO usuario VALUES ('{$matricula}', '{$curso}', '{$periodo}', '{$nome}', '{$email}', '{$senha}', '{$login}', '{$relat}')");
 
-        header("Location: ../login.php?success=true$linklog");
-
-    } elseif ($form == '2'){
-
-        $nome = $_POST['cadastro-tecnico-nome'];
-        $email = $_POST['cadastro-tecnico-email'];
-        $login = $_POST['cadastro-tecnico-login'];
-        $siape = $_POST['cadastro-tecnico-siape'];
-        $senha = $senha1;
-        $isTec = 0; // istec é o numero de reportes que o tecnico respondeu
-
-        $result = $pdo->query("INSERT INTO tecnico VALUES ('{$siape}', '{$nome}', '{$email}', '{$senha}', '{$login}', '{$isTec}')");
-
-        header("Location: ../login.php?success=true$linklog");
-    }
-
-echo "falha no sistema";
